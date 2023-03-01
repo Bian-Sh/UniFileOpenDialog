625e5f094846eb50868e4983fb39dc47753dee18
边上海 <bshsf@qq.com>
2023-03-01 18:59:31 +0800

    解决 ”fatal error: the current working directory was changed “ 异常

--- Assets/FileOpenDialog/FileDialogForWindows.cs
+++ Assets/FileOpenDialog/FileDialogForWindows.cs
@@ -1,10 +1,8 @@
-
-using UnityEngine;
-using System.Collections;
 using System;
 using System.Runtime.InteropServices;
 using System.Collections.Generic;
 using System.IO;
+using UnityEngine;
 
 public static class FileDialogForWindows
 {
@@ -42,9 +40,9 @@ public static class FileDialogForWindows
     [DllImport("user32.dll")]
     static extern IntPtr GetForegroundWindow();
     [DllImport("Comdlg32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
-    static extern bool GetOpenFileName([In, Out]DialogConfig dialog);  //这个方法名称必须为GetOpenFileName
+    static extern bool GetOpenFileName([In, Out] DialogConfig dialog);  //这个方法名称必须为GetOpenFileName
     [DllImport("Comdlg32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
-    static extern bool GetSaveFileName([In, Out]DialogConfig dialog);  //这个方法名称必须为GetSaveFileName
+    static extern bool GetSaveFileName([In, Out] DialogConfig dialog);  //这个方法名称必须为GetSaveFileName
     #endregion
 
     static string Filter(params string[] filters)
@@ -57,6 +55,7 @@ public static class FileDialogForWindows
     /// <param name="title">指定窗口名称</param>
     /// <param name="extensions">指定文件选择类型</param>
     /// <returns>选中的文件路径</returns>
+    /// https://learn.microsoft.com/zh-cn/windows/win32/api/commdlg/ns-commdlg-openfilenamea?redirectedfrom=MSDN
     public static string FileDialog(string title, params string[] extensions)
     {
         DialogConfig ofn = new DialogConfig();
@@ -74,10 +73,10 @@ public static class FileDialogForWindows
         ofn.maxFile = ofn.file.Length;
         ofn.fileTitle = new string(new char[64]);
         ofn.maxFileTitle = ofn.fileTitle.Length;
-        ofn.initialDir = UnityEngine.Application.dataPath;
+        ofn.initialDir = Application.dataPath;
         ofn.title = title;
         ofn.flags = 0x00080000 | 0x00001000 | 0x00000800 | 0x00000200 | 0x00000008;//OFN_EXPLORER|OFN_FILEMUSTEXIST|OFN_PATHMUSTEXIST| OFN_ALLOWMULTISELECT|OFN_NOCHANGEDIR
-        ofn.dlgOwner= GetForegroundWindow(); //这一步将文件选择窗口置顶。
+        ofn.dlgOwner = GetForegroundWindow(); //这一步将文件选择窗口置顶。
         if (!GetOpenFileName(ofn))
         {
             return null;
@@ -110,8 +109,8 @@ public static class FileDialogForWindows
         ofn.maxFileTitle = ofn.fileTitle.Length;
         ofn.initialDir = Path.GetDirectoryName(path);
         ofn.title = title;
-        ofn.flags = 0x00000002 | 0x00000004; // OFN_OVERWRITEPROMPT | OFN_HIDEREADONLY;
-        ofn.dlgOwner= GetForegroundWindow(); //这一步将文件选择窗口置顶。
+        ofn.flags = 0x00000002 | 0x00000004| 0x00000008; // OFN_OVERWRITEPROMPT | OFN_HIDEREADONLY;
+        ofn.dlgOwner = GetForegroundWindow(); //这一步将文件选择窗口置顶。
         if (!GetSaveFileName(ofn))
         {
             return null;
--- Assets/FileOpenDialog/OpenFileByWin32.cs
+++ Assets/FileOpenDialog/OpenFileByWin32.cs
@@ -1,8 +1,4 @@
-
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System.Runtime.InteropServices;
 using UnityEngine.UI;
 using System.IO;
 
